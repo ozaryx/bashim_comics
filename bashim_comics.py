fad403ae6aec6f095d4d34372c35887f917d2202
Маньков Константин Владимирович <k.mankov@cft.ru>
2019-02-20 23:58:39 +0700

    final release

--- bashim_comics.py
+++ bashim_comics.py
@@ -1,88 +1,124 @@
 import asyncio
 import aiohttp
-# import aiofiles
-# import json
+import aiofiles
+import os
 import re
-# import requests
 
 from bs4 import BeautifulSoup as BS
+from random import uniform
 from time import monotonic as mt
-"""
-1. Получить список ссылок на странички комиксов
-2. Получить ссылку на картинку комиксов
-3. Скачать картинки по ссылкам
-"""
 
-code = r'^<a href="(.*)"><.*'
-regexp = re.compile(code)
-
-URL_BASE = 'http://bash.im'
-URL = ''.join([URL_BASE, '/comics-calendar/{}'])
-proxies = {
-    'http': 'http://localhost:3128',
-    'https': 'http://localhost:3128',
-}
-
-proxy = 'http://localhost:3128'
+BASE_URL = 'https://bash.im{}'
+CALENDAR_URL = BASE_URL.format('/comics-calendar/{}')
 
 
-async def fetch_album(year, session):
-    global URL
-    url = URL.format(year)
-    async with session.get(url) as response:
-        if response.status == 200:
-            data = await response.json()
-            # response.text() - текст
-            # response.content() - файлы - картинки например
-            return data
-        print(f'Error fetching index {year}')
-        return None
+async def fetch_imgs(url, session, sleep_range):
+    """Получение и сохранение картинок комиксов
+    
+    url - адрес картинки
+    session - объект aiohttp.ClientSession
+    sleep_range - кортеж, диапазон задержки запуска корутины
+    """
+    await asyncio.sleep(uniform(*sleep_range))
+    try:
+        async with session.get(url) as response:
+            if response.status == 200:
+                data = await response.read()
+                filename = os.path.join('images/', url.rpartition('/')[-1])
+                try:
+                    async with aiofiles.open(filename, 'wb') as f:
+                        await f.write(data)
+                        print(f'Файл {filename} сохранен.')
+                except IOError:
+                    print(f'Не могу записать файл: {filename}')
+            else:
+                print(f'Ошибка загрузки изображения {url}. Ответ сервера: {response.status}')
+    except aiohttp.client_exceptions.ClientConnectorError:
+        print('Сбой подключения к серверу при обращении по адресу: {}'.format(url))
+        sleep_range = (5, 10)
+        async with aiohttp.ClientSession(trust_env=True) as session:
+            task = asyncio.create_task(fetch_imgs(url, session, sleep_range))
+            data = await asyncio.gather(task)
 
 
-async def fetch_html(year, session):
-    global URL
-    url = URL.format(year)
-    async with session.get(url) as response:
-        if response.status == 200:
-            data = await response.text()
-            # response.text() - текст
-            # response.content() - файлы - картинки например
-            soup = BS(data, "html.parser")
-            div = soup.find(id="calendar")
-            if div:
-                data = []
-                for elem in div.find_all('a'):
-                    data.append(regexp.match(str(elem)).group(1))
-            return data
-        print(f'Error fetching {url}. Response status: {response.status}')
-        return None
+async def fetch_html(url, session, sleep_range):
+    """Получение списка ссылок на странички картинок комиксов
+    и сами картинки
+    
+    url - адрес картинки
+    session - объект aiohttp.ClientSession
+    sleep_range - кортеж, диапазон задержки запуска корутины
+    """
+    await asyncio.sleep(uniform(*sleep_range))
+    try:
+        async with session.get(url) as response:
+            if response.status == 200:
+                data = await response.text()
+                data = BS(data, "html.parser")
+                # return data
+            else:
+                print(f'Ошибка при заросе {url}. Response status: {response.status}')
+                data = None
+    except aiohttp.client_exceptions.ClientConnectorError:
+        print(f'Сбой подключения к серверу при обращении по адресу: {url}')
+        data = None
+        # Попытка обработки сбоя подключения, но в итоге возвращается список в списке
+        # и обработка данных ломается
+        # sleep_range = (1, 5)
+        # async with aiohttp.ClientSession(trust_env=True) as session:
+        #     task = asyncio.create_task(fetch_html(url, session, sleep_range))
+        #     data = await asyncio.gather(task)
+    finally:
+        return data
 
 
 async def main(year):
-    # Получить список ссылок
+# Получение списка ссылок на странички комиксов
+    url = CALENDAR_URL.format(year)
+    sleep_range = (1, 2)
     async with aiohttp.ClientSession(trust_env=True) as session:
-        task = asyncio.create_task(fetch_html(year, session))
+        task = asyncio.create_task(fetch_html(url, session, sleep_range))
         data = await asyncio.gather(task)
-    print(data)
-    # filename = 'D:\\EDU\\PYTHON\\homework\\bashim_comics\\bash.im_comics-calendar_2018.html'
-    # async with aiofiles.open(filename, 'r', encoding='windows-1251') as f:
-    #     data = await f.read()
-    # print(data)
-    # print(div.find_all('a'))
 
+    soup = data[0]
+    if soup:
+        div = soup.find(id="calendar")
+        if div:
+            data = []
+            code = r'^<a href="(.*)"><.*'
+            regexp = re.compile(code)
+            for elem in div.find_all('a'):
+                data.append(regexp.match(str(elem)).group(1))
+            print(data[:10])
+        
+        urls = [BASE_URL.format(elem) for elem in data[:10]]
+        print(urls)
 
-#     <div id = "calendar" >
-# <a href = "/comics/20180101" > <img src = "https://bash.im/img/ts/uv2b5ewuuqllndzi448160.png" width = "40" height = "40" > </a >
-# </div >
+    # Установим диапазон случайной задержки для избежания шторма запросов на сервер
+        sleep_range = (2, 5)
+    # Получение списка ссылок на картинки комиксов
+        async with aiohttp.ClientSession(trust_env=True) as session:
+            tasks = [asyncio.create_task(fetch_html(url, session, sleep_range))
+                    for url in urls]
+            data = await asyncio.gather(*tasks)
 
-#     # Скачать файлы
-#     async with aiohttp.ClientSession() as session:
-#         tasks = [asyncio.create_task(fetch_album(i, session))
-#                  for i in range(1, 101)]
-#         data = await asyncio.gather(*tasks)
-#         # for d in data:
-#         #     if d:
-#         #         print(d['title'])
+        imgs = []
+        for soup in data:
+            if soup:
+                img = soup.find(id="cm_strip")
+                if img:
+                    code = r'^<img.*src="(.*)".*>'
+                    regexp = re.compile(code)
+                    data = regexp.match(str(img)).group(1)
+                    imgs.append(data)
+        print(imgs)
+
+    # Получение и сохранение картинок комиксов
+        async with aiohttp.ClientSession(trust_env=True) as session:
+            tasks = [asyncio.create_task(fetch_imgs(url, session, sleep_range))
+                    for url in imgs if url]
+            data = await asyncio.gather(*tasks)
+
 
 print('Asynchronous requests')
 t = mt()
